849926ee574ffc2cd56b5794a9cadef01c44fec1
1647392174 <883157594>
2021-07-12 11:07:24 +0800

    甜甜1

---
+++ meinian_common/src/test/java/com/atguigu/test/testDate.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class testDate {
+
+    @Test
+    public void testDate() {
+        System.out.println(new Date().getDay());
+    }
+}
